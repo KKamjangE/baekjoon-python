1cba3e042ab68b01aaf5395c47d26f70dbaada0e
KKamjangE <ajm980718@gmail.com>
2022-11-08 22:34:09 +0900

    22-11-08

--- 2주차/이분탐색/11053가장긴증가하는부분수열.py
+++ 2주차/이분탐색/11053가장긴증가하는부분수열.py
@@ -1,5 +1,16 @@
 import sys
-n = int(sys.stdin.readline())
+n = int(sys.stdin.readline()) # 수열의 개수
 
-a = list(map(int, sys.stdin.readline().split()))
+arr = list(map(int, sys.stdin.readline().split())) # 수열
 
+dp = [0 for _ in range(n)] # 길이를 넣는 배열
+
+for i in range(n): # index 차례대로
+    for j in range(i): # 현재 index의 이전 수열들을 비교하기 위한 for문
+        # 증가하는 수열인지 검사
+        if arr[i] > arr[j] and dp[i] < dp[j]: # 이전 원소보다 현재 원소가 크고 저장된 길이값이 작을 경우
+            dp[i] = dp[j] # 길이 복사
+    # 모든 길이는 초기값이 0
+    dp[i] += 1 # 길이 +
+    
+print(max(dp)) # 수열 중 가장 긴 길이를 출력
