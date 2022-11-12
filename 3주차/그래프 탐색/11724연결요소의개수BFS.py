d7d2f61eb3ffd02305c381d0aabbc86ab7024baf
KKamjangE <ajm980718@gmail.com>
2022-11-12 21:54:58 +0900

    22-11-12

--- 3주차/그래프 탐색/11724연결요소의개수BFS.py
+++ 3주차/그래프 탐색/11724연결요소의개수BFS.py
@@ -22,16 +22,16 @@ for _ in range(M): # 인접 리스트에 간선 추가
     graph[u].append(v)
     graph[v].append(u)
     
-visit = [False] * (N+1)
+visit = [False] * (N + 1)
     
 cnt = 0
     
-for i in range(1, N+1):
-    if visit[i] == False:
-        if not graph[i]:
+for i in range(1, N+1): # 다수의 그래프가 끊어져 있기 때문에 모든 노드를 검사
+    if visit[i] == False: # 방문 체크
+        if not graph[i]: # 해당 정점이 연결된 그래프가 없다면
             cnt += 1
-            visit[i] = True
-        else:
+            visit[i] = True # 방문 처리
+        else: # 연결된 그래프가 있다면
             bfs(i)
             cnt += 1
             
--- 3주차/그래프 탐색/2606바이러스BFS.py
+++ 3주차/그래프 탐색/2606바이러스BFS.py
@@ -2,30 +2,31 @@ import sys
 from collections import deque
 sys.setrecursionlimit(5000)
 
-def bfs(v):
+def bfs(v): # BFS
     global count
-    que = deque([v])
-    visit[v] = True
-    while que:
-        node = que.popleft()
-        for i in g[node]:
-            if not visit[i]:
-                que.append(i)
-                visit[i] = True
-                count += 1
+    que = deque([v]) # 큐 생성
+    visit[v] = True # 방문 처리
+    while que: # 큐가 빌 때 까지
+        node = que.popleft() # 맨 앞 원소를 제거해 node변수에 저장
+        for i in g[node]: # 해당 node에 연결된 간선들
+            if not visit[i]: # 방문 체크
+                que.append(i) # push
+                visit[i] = True # 방문 처리
+                count += 1 # 카운팅
 
-n = int(sys.stdin.readline())
-m = int(sys.stdin.readline())
+n = int(sys.stdin.readline()) # 정점의 개수
+m = int(sys.stdin.readline()) # 간선의 개수
 
-count = 0
-g = [[] for _ in range(n + 1)]
+count = 0 # 카운터
+g = [[] for _ in range(n + 1)] # 빈 인접 리스트 생성
 
-for _ in range(m):
+for _ in range(m): # 인접 리스트에 간선 저장
     x, y = map(int, sys.stdin.readline().split())
+    # 무방향이기 때문에 양쪽에 입력해준다
     g[x].append(y)
     g[y].append(x)
 
-visit = [False] * (n + 1)
+visit = [False] * (n + 1) # 방문 리스트 생성
 
-bfs(1)
+dfs(1) # 루트가 1로 주어진다
 print(count)
