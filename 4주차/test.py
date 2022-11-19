8bc286c9d3f604420aa8bf7114775d97ff3f3219
KKamjangE <ajm980718@gmail.com>
2022-11-19 22:56:21 +0900

    22-11-19

--- 4주차/test.py
+++ 4주차/test.py
@@ -1,8 +1,24 @@
-arr = input().split('-')
-s = 0
-for i in arr[0].split('+'):
-    s += int(i)
-for i in arr[1:]:
-    for j in i.split('+'):
-        s -= int(j)
-print(s)
+import sys
+
+N, K = map(int, input().split())
+stuff = [[0,0]]
+knapsack = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+
+for _ in range(N):
+    stuff.append(list(map(int, input().split())))
+
+
+#냅색 문제 풀이
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
+        weight = stuff[i][0] 
+        value = stuff[i][1]
+       
+        if j < weight:
+            knapsack[i][j] = knapsack[i - 1][j] #weight보다 작으면 위의 값을 그대로 가져온다
+        else:
+            knapsack[i][j] = max(value + knapsack[i - 1][j - weight], knapsack[i - 1][j])
+
+print(*knapsack, sep='\n')
+print(*stuff, sep='\n')
+print(knapsack[N][K])
---
+++ 4주차/동적 프로그래밍/12865평범한배낭.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split()) # 물건 개수, 배낭 무게 한도
+
+knapsack = [[0 for _ in range(K + 1)] for _ in range(N + 1)] # 그래프
+
+arr = [list(map(int, input().split())) for _ in range(N)] # 물건의 종류 배열
+
+# print(*arr, sep='\n')
+# print(*knapsack, sep='\n')
+
+for i in range(1, N + 1): # 물건의 개수만큼
+    for j in range(1, K + 1): # 가방의 무게 만큼
+        w = arr[i-1][0] # 물건 무게
+        v = arr[i-1][1] # 물건 가치
+        
+        if j < w: # 가방 무게가 물건의 무게보다 낮다면
+            knapsack[i][j] = knapsack[i - 1][j] # 전 물건의 가치 입력
+        else: # 물건의 무게가 더 크다면
+            # max(현재 물건 가치 + 현재 물건의 무게를 뺀 지점의 물건의 가치, 이전 물건의 가치)
+            knapsack[i][j] = max(v + knapsack[i - 1][j - w], knapsack[i - 1][j])
+
+print(knapsack[N][K]) # 최종 가치 출력
