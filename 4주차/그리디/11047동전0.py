dea388a3e7f5e5d024e772120a1d3f717b4d3736
KKamjangE <ajm980718@gmail.com>
2022-11-19 17:35:48 +0900

    22-11-19

--- 4주차/그리디/11047동전0.py
+++ 4주차/그리디/11047동전0.py
@@ -12,8 +12,9 @@ result = 0 # 동전 개수 카운팅
 for coin in coins: # 큰 동전 부터
     if K == 0: # 만들어야 하는 금액이 충촉 됐다면
         break # 중지
-    while coin <= K: # 동전이 금액보다 작을 때
-        K -= coin # 금액 - 동전
-        result += 1 # 동전 1개 추가
+    if coin <= K: # 동전이 금액보다 작을 때
+        n = K//coin # 금액이 동전 몇개로 나눠 지는지 저장
+        K = K%coin # 금액에서 동전을 나눈 나머지를 저장
+        result += n # 나눠진 개수 저장
         
 print(result)
